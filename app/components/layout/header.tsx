ec9f7046559acd65f143290e3a0ba0fa2d9c99ef
Slaytt <slaytran7@gmail.com>
2026-01-21 18:27:53 +0100

    change header's path

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Button } from '@/app/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 export function Header() {
